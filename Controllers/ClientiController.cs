e8d4e19e19010602d737e0ae95a3ed4ad4dcacc1
dimitrilzk <dimitrilazuka95@gmail.com>
2023-01-14 10:30:16 +0100

    clienti finito

--- Controllers/ClientiController.cs
+++ Controllers/ClientiController.cs
@@ -49,6 +49,120 @@ namespace Hotel.Controllers
 
             return RedirectToAction("AggiungiCliente");
         }
-
+        public ActionResult ModificaCliente(int id)
+        {
+            SqlConnection con = Connessione.GetConnection();
+            Clienti cliente = new Clienti();
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.CommandText = "select * from clienti where IdCliente = @id";
+                cmd.Connection=con;
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    cliente.IdCliente = Convert.ToInt32(reader["IdCliente"]);
+                    cliente.Nome = reader["Nome"].ToString();
+                    cliente.Cognome = reader["Cognome"].ToString();
+                    cliente.CodiceFiscale = reader["CodiceFiscale"].ToString();
+                    cliente.Citta = reader["Citta"].ToString();
+                    cliente.Email = reader["Email"].ToString();
+                    cliente.Cellulare = reader["Cellulare"].ToString();
+                }
+            }catch(Exception ex) 
+            { 
+                con.Close(); 
+            } 
+            finally 
+            { 
+                con.Close(); 
+            }
+            return View(cliente);
+        }
+        [HttpPost]
+        public ActionResult ModificaCliente(Clienti c, int id)
+        {
+            SqlConnection con = Connessione.GetConnection();
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Parameters.AddWithValue("@IdCliente", id);
+                cmd.Parameters.AddWithValue("@Nome", c.Nome);
+                cmd.Parameters.AddWithValue("@Cognome", c.Cognome);
+                cmd.Parameters.AddWithValue("@CodiceFiscale", c.CodiceFiscale);
+                cmd.Parameters.AddWithValue("@Citta", c.Citta);
+                cmd.Parameters.AddWithValue("@Email", c.Email);
+                cmd.Parameters.AddWithValue("@Cellulare", c.Cellulare);
+                cmd.CommandText = "update clienti set Nome = @Nome, Cognome = @Cognome, CodiceFiscale = @CodiceFiscale, Citta = @Citta," +
+                    "Email = @Email, Cellulare = @Cellulare where IdCliente = @IdCliente";
+                cmd.Connection = con;
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                con.Close();
+            } 
+            finally 
+            { 
+                con.Close(); 
+            }
+            return RedirectToAction("AggiungiCliente");
+        }
+        public ActionResult EliminaCliente(int id)
+        {
+            SqlConnection con = Connessione.GetConnection();
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.CommandText = "delete from clienti where IdCliente= @id";
+                cmd.Connection = con;
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                con.Close();
+            }
+            finally
+            { 
+                con.Close(); 
+            }
+            return RedirectToAction("AggiungiCliente");
+        }
+        public ActionResult DettagliCliente(int id)
+        {
+            SqlConnection con = Connessione.GetConnection();
+            Clienti cliente = new Clienti();
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.CommandText = "select * from clienti where IdCliente = @id";
+                cmd.Connection = con;
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    cliente.IdCliente = Convert.ToInt32(reader["IdCliente"]);
+                    cliente.Nome = reader["Nome"].ToString();
+                    cliente.Cognome = reader["Cognome"].ToString();
+                    cliente.CodiceFiscale = reader["CodiceFiscale"].ToString();
+                    cliente.Citta = reader["Citta"].ToString();
+                    cliente.Email = reader["Email"].ToString();
+                    cliente.Cellulare = reader["Cellulare"].ToString();
+                }
+            }catch(Exception ex)
+            {
+                con.Close();
+            }finally
+            {
+                con.Close();
+            }
+            return View(cliente);
+        }
     }
 }
